adce32921022281999e1ca2705f38d9a41c5efd5
Weljohn Catanpatan <36193712+Seydus@users.noreply.github.com>
2023-08-20 09:49:26 +0800

    0242-valid-anagram-short.cs

---
+++ 0242-valid-anagram/0242-valid-anagram-short.cs
@@ -0,0 +1,12 @@
+bool isAnagram(char * s, char * t){
+    int ls = strlen(s), lt = strlen(t);
+    if (ls != lt) return false;
+    int letters[26] = {0};
+
+    for (int i = 0; i < ls; i++) letters[s[i] - 'a'] += 1;
+    for (int i = 0; i < lt; i++) letters[t[i] - 'a'] -= 1;
+    for (int i = 0; i < 26; i++) {
+        if (letters[i] != 0) return false;
+    }
+    return true;
+}
