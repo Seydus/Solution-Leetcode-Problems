cdfee120e4eacda2480ee065287f7e8257277d2e
Weljohn Catanpatan <36193712+Seydus@users.noreply.github.com>
2023-08-20 23:51:21 +0800

    0242-valid-anagram-short.c

--- 0242-valid-anagram/0242-valid-anagram-short.c
+++ 0242-valid-anagram/0242-valid-anagram-short.c
@@ -2,7 +2,7 @@ bool isAnagram(char * s, char * t){
     int ls = strlen(s), lt = strlen(t);
     if (ls != lt) return false;
     int letters[26] = {0};
-
+    
     for (int i = 0; i < ls; i++) letters[s[i] - 'a'] += 1;
     for (int i = 0; i < lt; i++) letters[t[i] - 'a'] -= 1;
     for (int i = 0; i < 26; i++) {
