6648e96fe299f3b623292a9f101379f4d58079e7
Weljohn Catanpatan <36193712+Seydus@users.noreply.github.com>
2023-08-21 10:08:58 +0800

    Time: 7 ms (99.69%), Space: 6.8 MB (29.06%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.c
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.c
@@ -4,27 +4,24 @@
 #define SIZE 101
 
 int* smallerNumbersThanCurrent(int* nums, int numsSize, int* returnSize) {
-    int* hashmap = (int*)calloc(SIZE, sizeof(int));
+    int freq[SIZE] = {0};
     int* result = (int*)malloc(numsSize * sizeof(int));
     
     for (int i = 0; i < numsSize; i++) 
     {
-        hashmap[nums[i]]++;
+        freq[nums[i]]++;
+    }
+
+    for (int i = 1; i < SIZE; i++) 
+    {
+        freq[i] += freq[i - 1];
     }
     
     for (int i = 0; i < numsSize; i++) 
     {
-        int count = 0;
-        
-        for (int j = 0; j < nums[i]; j++) 
-        {
-            count += hashmap[j];
-        }
-
-        result[i] = count;
+        result[i] = nums[i] == 0 ? 0 : freq[nums[i] - 1];
     }
     
     *returnSize = numsSize;
-    free(hashmap);
     return result;
 }
